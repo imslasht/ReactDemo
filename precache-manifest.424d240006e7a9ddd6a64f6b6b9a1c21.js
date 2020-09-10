82f05a1b48f978dc002f88f50317f1b8567b14e5
Celeste Gupta <celestegupta7@gmail.com>
2020-09-10 14:20:20 -0400

    Updates

---
+++ precache-manifest.424d240006e7a9ddd6a64f6b6b9a1c21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9a8c08f6e8ada0e8ec696084606fcb",
+    "url": "/ReactDemo/index.html"
+  },
+  {
+    "revision": "3757b73c0ef1d112c122",
+    "url": "/ReactDemo/static/css/2.e9e1f360.chunk.css"
+  },
+  {
+    "revision": "b82eb0c0621ee69b1bfb",
+    "url": "/ReactDemo/static/css/main.66132540.chunk.css"
+  },
+  {
+    "revision": "3757b73c0ef1d112c122",
+    "url": "/ReactDemo/static/js/2.8a13ced0.chunk.js"
+  },
+  {
+    "revision": "6aab4016e48f22597423e62d1fc4ca26",
+    "url": "/ReactDemo/static/js/2.8a13ced0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82eb0c0621ee69b1bfb",
+    "url": "/ReactDemo/static/js/main.6a10d757.chunk.js"
+  },
+  {
+    "revision": "152a3f44e2b992e27051",
+    "url": "/ReactDemo/static/js/runtime-main.c2d2f420.js"
+  },
+  {
+    "revision": "0bad57e53d9f54a19a386d314afec45f",
+    "url": "/ReactDemo/static/media/smart-icons.0bad57e5.ttf"
+  },
+  {
+    "revision": "4356ae32b177373513fb8419b03af504",
+    "url": "/ReactDemo/static/media/smart-icons.4356ae32.woff"
+  },
+  {
+    "revision": "4d447d4a6aed96848fcc1d422a7e51b8",
+    "url": "/ReactDemo/static/media/smart-icons.4d447d4a.woff2"
+  },
+  {
+    "revision": "8c3af8d93a272650587a16879ea65872",
+    "url": "/ReactDemo/static/media/smart-icons.8c3af8d9.eot"
+  }
+]);
